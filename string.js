6d511fa4466eefc83e6cbd59121a990309340e10
ahrimy <dlsdjdkfla@gmail.com>
2020-10-21 19:29:12 +0900

    string: ES2015 String에 새로운 메서드들

---
+++ string.js
@@ -0,0 +1,7 @@
+//ES2015 string에 새로운 메서드들.
+
+let str = 'hello world ? ^^ ~~';
+let matchstr = 'hello';
+console.log('startsWith', str.startsWith(matchstr));
+console.log('endsWith', str.endsWith(matchstr));
+console.log('includes', str.includes('world'));
