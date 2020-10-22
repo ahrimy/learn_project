8aeb27f71e74936b4b2160b34002a4a32d46c36c
ahrimy <dlsdjdkfla@gmail.com>
2020-10-22 23:14:19 +0900

    array: for of - 순회하기

---
+++ Array/for-of.js
@@ -0,0 +1,28 @@
+var data = [1, 2, undefined, NaN, null, ''];
+
+console.log('[for]');
+for (var i = 0; i < data.length; i++) {
+  console.log(i);
+}
+
+console.log('[forEach]');
+data.forEach(function (value) {
+  console.log('value is', value);
+});
+
+console.log('[for-in]');
+Array.prototype.getIndex = function () {};
+for (let idx in data) {
+  console.log(data[idx]);
+}
+//for in 의 경우 자기자신의 데이터 이외에 상위의 값을 읽어온다.
+
+console.log('[for-of]');
+for (let value of data) {
+  console.log(value);
+}
+
+var str = 'hello world!!!!';
+for (let value of str) {
+  console.log(value);
+}
