f58fad0917247213545616d36e86bac587238926
ahrimy <dlsdjdkfla@gmail.com>
2020-10-23 19:14:51 +0900

    practice1: 해설

--- practice/practice1/practice1.js
+++ practice/practice1/practice1.js
@@ -3,11 +3,17 @@ function print() {
    * filter, includes, from을 사용해서 문자열'e'기 포함된 배열을 만들어서 반환하기
    */
   const nodeList = document.querySelectorAll('li');
-  const list = Array.from(nodeList);
+  //toString.call() 타입 반환
+  console.log(toString.call(nodeList));
+
+  const list = Array.from(nodeList); //li노드로 구성된 배열
+  console.log(toString.call(list));
+
   const filteredList = list.filter(function (value) {
-    return value.textContent.includes('e');
+    //value는 노드
+    return value.innerText.includes('e');
   });
-  console.log(filteredList);
+  return filteredList;
 }
 
-print();
+console.log(print());
