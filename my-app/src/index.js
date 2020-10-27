af34afaad3fecc88f9364903a99775c445aa449e
ahrimy <dlsdjdkfla@gmail.com>
2020-10-28 00:28:40 +0900

    feat: add click option
    add 'X' when the square is clicked

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,14 +3,42 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+  constructor(props) {
+    //모든 React.Component 클래스는 생성자를 가질 때 super(props) 호출 필수
+    super(props);
+    this.state = { value: null };
+  }
   render() {
-    return <button className="square">{/* TODO */}</button>;
+    return (
+      <button
+        className="square"
+        onClick={() => this.props.onClick({ value: 'X' })}
+      >
+        {this.props.value}
+      </button>
+    );
   }
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null)
+    };
+  }
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({ squares: squares });
+  }
   renderSquare(i) {
-    return <Square />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
