8a85baa4a1a0bb8a3bff0623fa67a61cd513348e
ahrimy <dlsdjdkfla@gmail.com>
2020-11-01 14:18:37 +0900

    chore: setup to study css
    - setup files to test css

--- cra-test/src/App.js
+++ cra-test/src/App.js
@@ -1,26 +1,18 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from "react";
+import Button from "./Button1";
+import Box from "./Box1";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>hello</p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Button size="big" />
+        <Button size="small" />
+        <Box size="big" />
+        <Box size="small" />
+      </div>
+    );
+  }
 }
 
 export default App;
