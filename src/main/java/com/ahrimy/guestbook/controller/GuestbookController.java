81a6c160c71755c0d22261e6b02f1219e42b7206
Ahrim Yang <ahrim.yang@gmail.com>
2022-01-11 16:59:58 +0900

    [feat] 목록 조회 화면 추가, GuestbookController 기능 추가

--- src/main/java/com/ahrimy/guestbook/controller/GuestbookController.java
+++ src/main/java/com/ahrimy/guestbook/controller/GuestbookController.java
@@ -1,18 +1,32 @@
 package com.ahrimy.guestbook.controller;
 
+import com.ahrimy.guestbook.dto.PageRequestDTO;
+import com.ahrimy.guestbook.service.GuestbookService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/guestbook")
 @Log4j2
+@RequiredArgsConstructor
 public class GuestbookController {
 
-    @GetMapping({"/", "/list"})
-    public String list() {
-        log.info("list..........");
-        return "/guestbook/list";
+    private final GuestbookService service;
+
+    @GetMapping("/")
+    public String index() {
+        return "redirect:/guestbook.list";
+    }
+
+    @GetMapping("/list")
+    public void list(PageRequestDTO pageRequestDTO, Model model) {
+
+        log.info("list.........." + pageRequestDTO);
+
+        model.addAttribute("result", service.getList(pageRequestDTO));
     }
 }
