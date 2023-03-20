6a53d074ce956a90b39fbd15e4da3a374d942457
Ahrim Yang <ahrim.yang@nhnpayco.com>
2023-03-20 11:56:45 +0900

    Create Entity class

--- src/main/java/jpabook/jpashop/domain/Delivery.java
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -13,6 +13,7 @@ public class Delivery {
     @Id
     @GeneratedValue
     @Column(name = "delivery_id")
+    private Long id;
 
     @OneToOne(mappedBy = "delivery")
     private Order order;
--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,5 +1,8 @@
 package jpabook.jpashop.domain.item;
 
+import java.util.ArrayList;
+import java.util.List;
+import jpabook.jpashop.domain.Category;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -20,4 +23,7 @@ public abstract class Item {
     private String name;
     private int price;
     private int stockQuantity;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
 }
