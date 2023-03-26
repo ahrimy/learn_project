c4fd4f7eb0d8717faffa9cc500cccf5461d7eb06
Ahrim Yang <ahrim.yang@nhnpayco.com>
2023-03-26 12:51:38 +0900

    Member Repository, Member Service

---
+++ src/main/java/jpabook/jpashop/domain/service/MemberService.java
@@ -0,0 +1,42 @@
+package jpabook.jpashop.domain.service;
+
+import java.util.List;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository repository;
+
+    // 회원 가입
+    @Transactional
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        repository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> members = repository.findByName(member.getName());
+        if (!members.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    // 회원 전체 조회
+//    @Transactional(readOnly = true) // 성능적인 장점이 있음
+    public List<Member> findMember() {
+        return repository.findAll();
+    }
+
+    public Member findOne(Long id) {
+        return repository.findOne(id);
+    }
+}
