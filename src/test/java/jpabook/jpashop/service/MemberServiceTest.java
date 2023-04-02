33a3716e86d55e0af239658040d34e9b1aa9c853
Ahrim Yang <dlsdjdkfla@gmail.com>
2023-04-02 19:39:00 +0900

    Move Service package

--- src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -1,4 +1,4 @@
-package jpabook.jpashop.domain.service;
+package jpabook.jpashop.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
